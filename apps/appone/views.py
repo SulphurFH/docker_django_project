3b618d001a13774a28695a402fdb9e7736e3411f
FangHao <fanghao9342@gmail.com>
2017-07-30 00:31:19 +0800

    fix bug

--- apps/appone/views.py
+++ apps/appone/views.py
@@ -7,7 +7,7 @@ def first_api_view(request):
                 'name': 'docker_django_project',
                 'version': '0.1v',
                 'use': 'test',
-                'test_one': 'add response,test git pull'
+                'test_one': 'add response,test git pull',
                 'test_two': 'test container git pull'
             }
     return JsonResponse(context)
