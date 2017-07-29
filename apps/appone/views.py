9e15b06bfa1f992d8d16adb99538660df52a2da1
FangHao <fanghao9342@gmail.com>
2017-07-30 00:44:04 +0800

    test control

--- apps/appone/views.py
+++ apps/appone/views.py
@@ -7,8 +7,5 @@ def first_api_view(request):
                 'name': 'docker_django_project',
                 'version': '0.1v',
                 'use': 'test',
-                'test_one': 'add response,test git pull',
-                'test_two': 'test container git pull',
-                'test_three': 'test container git pull',
             }
     return JsonResponse(context)
